08fb689b31e7e4fc8c51681ea6286281240db900
Kevin Welch <kaedub16@gmail.com>
2018-10-25 11:32:29 -0700

    Create jobRoutes file

---
+++ routes/jobRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = new express.Router();
+const { validate } = require('jsonschema');
+
+router.get("/", async function (req, res, next) {
+});
+
+module.exports = router;
