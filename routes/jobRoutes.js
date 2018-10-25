9497229cf1b3df8e311b613c66b58e1a01ad3849
Lulzasaur <frost9192@gmail.com>
2018-10-25 14:01:56 -0700

    deleted conflicting files

--- routes/jobRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = new express.Router();
-const { validate } = require('jsonschema');
-
-router.get("/", async function (req, res, next) {
-});
-
-module.exports = router;
