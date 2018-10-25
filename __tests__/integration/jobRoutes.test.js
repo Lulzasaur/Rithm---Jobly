0c7f44a7c93dfb011ee0a2c03cb0f064185fe022
Kevin Welch <kaedub16@gmail.com>
2018-10-25 14:36:53 -0700

    getAll tests passed

--- __tests__/integration/jobRoutes.test.js
+++ __tests__/integration/jobRoutes.test.js
@@ -8,7 +8,7 @@ const AAPL_JOB = {
 	title: "CEO",
 	salary: 210 * 1000,
 	equity: 0.7,
-	company_handle: "aapl"
+	company_handle: "AAPL"
 };
 
 const DPW_JOB = {
@@ -16,7 +16,7 @@ const DPW_JOB = {
 	title: "CTO",
 	salary: 120 * 1000,
 	equity: 0.3,
-	company_handle: "dpw",
+	company_handle: "DPW",
 };
 
 /***********************/
@@ -24,18 +24,47 @@ const DPW_JOB = {
 
 // set up table
 beforeAll(async () => {
+  
+	await db.query(`CREATE TABLE companies (
+		handle TEXT PRIMARY KEY,
+		name TEXT NOT NULL UNIQUE,
+		num_employees INTEGER,
+		description TEXT, 
+    logo_url TEXT)`
+  );
+  
 	await db.query(`CREATE TABLE jobs (
 			id SERIAL PRIMARY KEY,
 			title TEXT NOT NULL,
 			salary FLOAT NOT NULL,
 			equity FLOAT NOT NULL CHECK (equity<1), 
 			company_handle TEXT REFERENCES companies(handle),
-			date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP`
+			date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`
 	);
 });
 
 beforeEach(async () => {
 // seed with some data
+
+  await db.query(`INSERT INTO companies (
+    handle,
+    name,
+    num_employees,
+    description,
+    logo_url) 
+    VALUES ('AAPL','Apple Inc','10000','Computer maker','www.apple.com') `
+  );
+
+  await db.query(`INSERT INTO companies (
+    handle,
+    name,
+    num_employees,
+    description,
+    logo_url) 
+    VALUES ('DPW','Digital Power Corp','10','Snake oil salesmen','www.dpw.com')`
+  );
+
+
 	await db.query(
 		`INSERT INTO jobs (title,salary,equity,company_handle)
 		VALUES ($1, $2, $3, $4)`,
@@ -50,11 +79,13 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-	await db.query("DELETE FROM jobs");
+  await db.query("DELETE FROM jobs");
+  await db.query("DELETE FROM companies");
 });
 
 afterAll(async () => {
-	await db.query("DROP TABLE jobs");
+  await db.query("DROP TABLE jobs");
+  await db.query("DROP TABLE companies");
 	db.end();
 });
 
@@ -66,85 +97,82 @@ describe("GET /", () => {
 
 	test("Sould respond with object with array of jobs", async() => {
 		const response = await request(app).get('/jobs');
-		console.log(response);
 
-		expect(response.status).toEqual(200);
+
+    expect(response.status).toEqual(200);
 		expect(response.body.jobs.length).toEqual(2);
-		expect(response.body.jobs[0].company_handle).toEqual('aapl');
-		expect(response.body.jobs[1].company_handle).toEqual('dpw');
+		expect(response.body.jobs[0].company_handle).toEqual('AAPL');
+		expect(response.body.jobs[1].company_handle).toEqual('DPW');
 	});
 });
 
-/****************/
-/** GET a job */
+// /****************/
+// /** GET a job */
 
-describe("GET /jobs", () => {
+// describe("GET /jobs", () => {
 
-	test("Should respond with object with array of jobs", async() => {
-		let jobID = 1;
-		const response = await request(app).get(`/jobs/${jobID}`);
-		console.log(response.job);
+// 	test("Should respond with object with array of jobs", async() => {
+// 		let jobID = 1;
+// 		const response = await request(app).get(`/jobs/${jobID}`);
 
-		expect(response.status).toEqual(200);
-		expect(response.body.job).toEqual(2);
-		expect(response.body.job.company_handle).toEqual('aapl');
-		expect(response.body.job.title).toEqual('ceo');
-	});
-});
+// 		expect(response.status).toEqual(200);
+// 		expect(response.body.job).toEqual(2);
+// 		expect(response.body.job.company_handle).toEqual('aapl');
+// 		expect(response.body.job.title).toEqual('ceo');
+// 	});
+// });
 
-/*******************/
-/** POST a new job */
+// /*******************/
+// /** POST a new job */
 
-describe('POST /jobs', () => {
-	test("Should respond with the new job fields", async() => {
-		let data = {
-			title: "CFO",
-			salary: 115000,
-			equity: 0.4,
-			company_handle: 'aapl'
-		}
+// describe('POST /jobs', () => {
+// 	test("Should respond with the new job fields", async() => {
+// 		let data = {
+// 			title: "CFO",
+// 			salary: 115000,
+// 			equity: 0.4,
+// 			company_handle: 'aapl'
+// 		}
 
-		const response = await request(app).post('/jobs');
-		console.log(response.job);
+// 		const response = await request(app).post('/jobs');
 
-		expect(response.status).toEqual(200);
-		expect(response.body.job.title).toEqual(data.title);
-		expect(response.body.job.salary).toEqual(data.salary);
-		expect(response.body.equity).toEqual(data.equity);
-		expect(response.body.company_handle).toEqual(data.company_handle);
-	})
-});
+// 		expect(response.status).toEqual(200);
+// 		expect(response.body.job.title).toEqual(data.title);
+// 		expect(response.body.job.salary).toEqual(data.salary);
+// 		expect(response.body.equity).toEqual(data.equity);
+// 		expect(response.body.company_handle).toEqual(data.company_handle);
+// 	})
+// });
 
-/*******************/
-/** UPDATE a job */
+// /*******************/
+// /** UPDATE a job */
 
-describe('PATCH /jobs/:id', () => {
-	test("Should respond with the updated job fields", async() => {
-		let id = 2;
-		let data = {
-			title: 'Software engineer',
-			salary: 90000
-		};
+// describe('PATCH /jobs/:id', () => {
+// 	test("Should respond with the updated job fields", async() => {
+// 		let id = 2;
+// 		let data = {
+// 			title: 'Software engineer',
+// 			salary: 90000
+// 		};
 
-		const response = await request(app).patch(`/jobs/${id}`);
-		console.log(response.job);
+// 		const response = await request(app).patch(`/jobs/${id}`);
 
-		expect(response.status).toEqual(200);
-		expect(response.body.job.title).toEqual(data.title);
-		expect(response.body.job.salary).toEqual(data.salary);
-	});
-});
+// 		expect(response.status).toEqual(200);
+// 		expect(response.body.job.title).toEqual(data.title);
+// 		expect(response.body.job.salary).toEqual(data.salary);
+// 	});
+// });
 
-/*******************/
-/** DELETE a job */
+// /*******************/
+// /** DELETE a job */
 
-describe('DELETE /jobs/:id', () => {
-	test("Should respond with a message 'Job deleted'", async() => {
-		let id = 1;
+// describe('DELETE /jobs/:id', () => {
+// 	test("Should respond with a message 'Job deleted'", async() => {
+// 		let id = 1;
 
-		const response = await request(app).delete(`/jobs/${id}`);
+// 		const response = await request(app).delete(`/jobs/${id}`);
 
-		expect(response.body.message).toEqual('Job deleted');
-		expect(response.status).toEqual(200);
-	});
-})
+// 		expect(response.body.message).toEqual('Job deleted');
+// 		expect(response.status).toEqual(200);
+// 	});
+// })
